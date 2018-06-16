e1194d1ea70c157a19faa1955de6002fc8198924
singhutsav5502 <singhutsavofficial@gmail.com>
2018-06-16 11:56:55 +0530

    Update NestedArrays.py

--- NestedArrays.py
+++ NestedArrays.py
@@ -1,12 +1,12 @@
 A=["1","[2,2.5]","3","4"]
 x= " "
 for i in range(len(A)):
-    if A[i].startswith("[") and A[i].endswith("]"):
-        print("found a list in the list on element:",i)
+    if A[i].startswith("[") and A[i].endswith("]"): #checking if the element is an array using a string method
+        print("found a list in the list on element:",i) 
         internalList=A[i]
-        for j in internalList:
-            print('elements are:',j)
-        '''x=A[i].replace("["," ")
+        for j in internalList: #trying to print individual elements of the array but DOESN'T work due to string type
+            print('elements are:',j) 
+        '''x=A[i].replace("["," ") #A test code to strip out values from a nested array
         x=x.replace("]"," ")
         x= x.replace(","," ")
         x = x.strip()
